06fa38d6c52f08193db578932edc1063629ba22f
devforfu <developer.z@outlook.com>
2019-12-01 18:48:50 +0500

    Init

---
+++ basedir.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+DATA = Path.home()/'data'/'bowl2019'
+TRAIN = DATA/'train.csv'
+TEST = DATA/'test.csv'
+TRAIN_SPECS = DATA/'specs.csv'
+TRAIN_LABELS = DATA/'train_labels.csv'
+SAMPLE = DATA/'sample_submission.csv' 
+
